6fd0f2195701a55d00ddc20487affd7e09815171
Luis Fernando Domingo Gómez <al222010066@gmail.com>
2022-08-30 14:17:46 -0500

    Modulo de visualizacion

--- resources/views/gallo/edit.blade.php
+++ resources/views/gallo/edit.blade.php
@@ -5,28 +5,47 @@
 @endsection
 
 @section('content')
-<br><br><br><br>
-    <section class="content container-fluid">
-        <div class="">
-            <div class="col-md-12">
+    <!-- Header -->
+    <div class="header bg-primary pb-6">
+        <div class="container-fluid">
+            <div class="header-body">
+            <div class="row align-items-center py-4">
+                <div class="col-lg-6 col-7">
+                <h6 class="h2 text-white d-inline-block mb-0">Gallos</h6>
+                <nav aria-label="breadcrumb" class="d-none d-md-inline-block ml-md-4">
+                    <ol class="breadcrumb breadcrumb-links breadcrumb-dark">
+                    <li class="breadcrumb-item"><a href="#"><i class="fas fa-home"></i></a></li>
+                    <li class="breadcrumb-item"><a href="#">Gallos</a></li>
+                    <li class="breadcrumb-item active" aria-current="page">Participantes</li>
+                    </ol>
+                </nav>
+                </div>
+            </div>
+            </div>
+        </div>
+        <br>
+        <section class="content container-fluid">
+            <div class="">
+                <div class="col-md-12">
 
-                @includeif('partials.errors')
+                    @includeif('partials.errors')
 
-                <div class="card card-default">
-                    <div class="card-header">
-                        <span class="card-title">Update Gallo</span>
-                    </div>
-                    <div class="card-body">
-                        <form method="POST" action="{{ route('gallos.update', $gallo->id) }}"  role="form" enctype="multipart/form-data">
-                            {{ method_field('PATCH') }}
-                            @csrf
+                    <div class="card card-default">
+                        <div class="card-header">
+                            <span class="card-title">Update Gallo</span>
+                        </div>
+                        <div class="card-body">
+                            <form method="POST" action="{{ route('gallos.update', $gallo->id) }}"  role="form" enctype="multipart/form-data">
+                                {{ method_field('PATCH') }}
+                                @csrf
 
-                            @include('gallo.form')
+                                @include('gallo.form')
 
-                        </form>
+                            </form>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-    </section>
-@endsection
+        </section>
+    @endsection
+</div>
