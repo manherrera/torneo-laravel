d81bb5cf7d73f51f0346c72f9ee8fccc6769b65a
Luis Fernando Domingo Gomez <al222010066@gmail.com>
2022-09-15 23:14:54 -0500

    Seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -1,6 +1,7 @@
 <?php
 namespace Database\Seeders;
 
+use App\Models\Partido;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Database\Seeder;
 
@@ -13,6 +14,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call([UsersTableSeeder::class]);
+        $this->call([
+            UsersTableSeeder::class,
+            PartidoSeeder::class,
+        ]);
     }
 }
